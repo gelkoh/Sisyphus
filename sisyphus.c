eb0e121dbd25675e9145d14bd2ec9fd854d620c9
Gelkoh <ole_kirchner@gmx.de>
2025-03-13 19:09:58 +0100

    Add function to import and activate styles

--- sisyphus.c
+++ sisyphus.c
@@ -2,13 +2,26 @@
 #include <stdio.h>
 #include <gtk/gtk.h>
 
+static void activate_styles() {
+    GtkCssProvider *css_provider = gtk_css_provider_new();
+    gtk_css_provider_load_from_path(css_provider, "styles.css");
+    gtk_style_context_add_provider_for_display(
+        gdk_display_get_default(),
+        GTK_STYLE_PROVIDER(css_provider),
+        GTK_STYLE_PROVIDER_PRIORITY_USER
+    );
+}
+
 static void activate(GtkApplication *app) {
     GtkWidget *window;
 
     window = gtk_application_window_new(app);
+    gtk_widget_set_name(window, "window");
     gtk_window_set_title(GTK_WINDOW(window), "🏺 Sisyphus – A Pomodoro Timer 🏺");
     gtk_widget_set_size_request(window, 400, 400);
     gtk_window_present(GTK_WINDOW(window));
+
+    activate_styles();
 }
 
 int main(int argc, char **argv) {
